162206901991106d760c78d016fb651dc6f5bafb
12herman <josephherman810@gmail.com>
2025-04-24 15:20:47 +0530

    errorfix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 import cesium from 'vite-plugin-cesium';
-import ghPages from 'vite-plugin-gh-pages';
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/cesium-test-app/',
-  plugins: [react(),tailwindcss(),cesium(),ghPages()],
+  plugins: [react(),tailwindcss(),cesium()],
 })
