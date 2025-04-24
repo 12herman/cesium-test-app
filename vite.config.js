192f486550af9ecbd79d353349dd2719707e21da
12herman <josephherman810@gmail.com>
2025-04-24 15:18:44 +0530

    partial update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 import cesium from 'vite-plugin-cesium';
-
+import ghPages from 'vite-plugin-gh-pages';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),cesium()],
+  base: '/cesium-test-app/',
+  plugins: [react(),tailwindcss(),cesium(),ghPages()],
 })
